d47afa3290dea2cc41b4135a821c256caddbda53
Ashish Khokhariya <akhokhariya@argusoft.in>
2021-09-27 21:49:21 +0530

    Added nodemon script and public file

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} `)
+})
